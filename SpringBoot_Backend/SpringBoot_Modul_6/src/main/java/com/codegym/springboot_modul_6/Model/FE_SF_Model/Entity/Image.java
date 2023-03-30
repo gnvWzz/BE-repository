5504da448734ab74e8354e3f1a40de6ea78cf1ab
gnvWzz <quangvuongdac2111@gmail.com>
2023-03-30 10:11:42 +0700

    Quang sửa Product.java và Image.java

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Model/FE_SF_Model/Entity/Image.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Model/FE_SF_Model/Entity/Image.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "images")
+@Table(name = "image")
 @Getter
 @Setter
 @NoArgsConstructor
--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Model/FE_SF_Model/Entity/Product.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Model/FE_SF_Model/Entity/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "products")
+@Table(name = "product")
 @Getter
 @Setter
 @NoArgsConstructor
