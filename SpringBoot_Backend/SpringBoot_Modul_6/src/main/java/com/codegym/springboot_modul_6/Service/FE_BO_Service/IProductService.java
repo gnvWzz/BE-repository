f68b75f59beb09e80ac8c0523ae8edb21d59fddb
TUNGHUYNH <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2023-03-31 17:05:07 +0700

    Tung add BE classes

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service/FE_BO_Service/IProductService.java
+++
@@ -1,4 +0,0 @@
-package com.codegym.springboot_modul_6.Service.FE_BO_Service;
-
-public interface IProductService {
-}
--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service/FE_BO_Service/imp/ProductServiceImp.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.springboot_modul_6.Service.FE_BO_Service.imp;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductServiceImp {
-
-}
