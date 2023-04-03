eff3f4a6b3abd776e29a3c368025e1dfe639fe4f
nguyenhai <nguyenhai190497@gmail.com>
2023-04-03 08:31:47 +0700

    Hải: Tạo Controller AccountController.java

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service/FE_SF_Service/ILoginService.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.springboot_modul_6.Service.FE_SF_Service;
-
-import com.codegym.springboot_modul_6.Model.FE_SF_Model.Entity.Account;
-
-public interface ILoginService extends IGeneralService<Account> {
-}
