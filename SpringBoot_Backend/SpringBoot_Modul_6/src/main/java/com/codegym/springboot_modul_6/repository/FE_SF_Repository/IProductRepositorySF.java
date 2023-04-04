a42181f45698f3de12c526ced1e1c75bba198e82
Long Hoang <longhoang12212@gmail.com>
2023-04-04 14:31:28 +0700

    Long sua conflict

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/repository/FE_SF_Repository/IProductRepositorySF.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/repository/FE_SF_Repository/IProductRepositorySF.java
@@ -1,5 +1,7 @@
 package com.codegym.springboot_modul_6.repository.FE_SF_Repository;
 
+
+
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.ProductSF;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/FE_SF_Service/IProductService.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/FE_SF_Service/IProductService.java
@@ -1,11 +1,9 @@
 package com.codegym.springboot_modul_6.service.FE_SF_Service;
 
-<<<<<<< HEAD:SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/FE_SF_Service/IProductService.java
+
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.ProductSF;
-=======
-import com.codegym.springboot_modul_6.Model.FE_SF_Model.Entity.ProductSF;
+
 import org.springframework.data.domain.Page;
->>>>>>> 815825339867bd4ca60d12e7bb6781ce959ee491:SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service/FE_SF_Service/IProductService.java
 
 import java.util.List;
 
