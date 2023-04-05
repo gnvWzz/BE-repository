5c49f2fb5168ca13c1df15f7821641c95069e1d7
LongC09 <98075045+HCL2001@users.noreply.github.com>
2023-04-05 08:17:54 +0700

    Delete SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service directory

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/Service/FE_SF_Service/ICategoriesService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.springboot_modul_6.service.FE_SF_Service;
-
-import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.Categories;
-
-public interface ICategoriesService extends IGeneralService<Categories>{
-
-
-
-}
