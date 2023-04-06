308c59d836db0bada71285d721eca85ba134dfc4
Long Hoang <longhoang12212@gmail.com>
2023-04-06 07:42:05 +0700

    Long commmit dau ngay

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/util/FE_SF_Util/Mapper/LongMapper.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/util/FE_SF_Util/Mapper/LongMapper.java
@@ -1,6 +1,5 @@
 package com.codegym.springboot_modul_6.util.FE_SF_Util.Mapper;
 
-import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.Image;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.ProductSF;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.ProductSFDetail;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.dto.ImageDto;
@@ -10,15 +9,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class LongMapper {
 
-    private List<ImageDto> mapperImageDtoList(ProductSF productSFS){
+    private List<ImageDto> mapperImageDtoList(ProductSF productSFS) {
         List<ImageDto> imageDtos = new ArrayList<>();
-        for(int i = 0; i < productSFS.getProductDetail().get(0).getImageList().size(); i++){
+        for (int i = 0; i < productSFS.getProductDetail().get(0).getImageList().size(); i++) {
             ImageDto url = new ImageDto();
             BeanUtils.copyProperties(productSFS.getProductDetail().get(0).getImageList().get(i), url);
             imageDtos.add(url);
@@ -26,9 +24,9 @@ public class LongMapper {
         return imageDtos;
     }
 
-    private List<ImageDto> mapperImageDto(ProductSFDetail productSFDetail){
+    private List<ImageDto> mapperImageDto(ProductSFDetail productSFDetail) {
         List<ImageDto> imageDtoss = new ArrayList<>();
-        for(int i = 0; i < productSFDetail.getImageList().size(); i++){
+        for (int i = 0; i < productSFDetail.getImageList().size(); i++) {
             ImageDto url = new ImageDto();
             BeanUtils.copyProperties(productSFDetail.getImageList().get(i), url);
             imageDtoss.add(url);
@@ -36,9 +34,9 @@ public class LongMapper {
         return imageDtoss;
     }
 
-    private List<ProductSFDetailDto> mapperProductSFDetailDto(ProductSF productSFS){
+    private List<ProductSFDetailDto> mapperProductSFDetailDto(ProductSF productSFS) {
         List<ProductSFDetailDto> productSFDetailDtos = new ArrayList<>();
-        for(int i = 0; i < productSFS.getProductDetail().size(); i++){
+        for (int i = 0; i < productSFS.getProductDetail().size(); i++) {
             ProductSFDetailDto productSFDetailDto = new ProductSFDetailDto();
             BeanUtils.copyProperties(productSFS.getProductDetail().get(i), productSFDetailDto);
             productSFDetailDto.setImageList(mapperImageDto(productSFS.getProductDetail().get(i)));
@@ -47,9 +45,9 @@ public class LongMapper {
         return productSFDetailDtos;
     }
 
-    public List<ProductSFDto> mapperProductSFDto(List<ProductSF> productSFS){
+    public List<ProductSFDto> mapperProductSFDto(List<ProductSF> productSFS) {
         List<ProductSFDto> productSFDtos = new ArrayList<>();
-        int i = 0;
+
         for (ProductSF p : productSFS
         ) {
             try {
@@ -57,14 +55,13 @@ public class LongMapper {
                 BeanUtils.copyProperties(p, productSFDto);
                 productSFDto.setProductDetail(mapperProductSFDetailDto(p));
                 productSFDtos.add(productSFDto);
-                i++;
+
             } catch (Exception e) {
                 throw e;
             }
         }
         return productSFDtos;
     }
-
 
 
 }
