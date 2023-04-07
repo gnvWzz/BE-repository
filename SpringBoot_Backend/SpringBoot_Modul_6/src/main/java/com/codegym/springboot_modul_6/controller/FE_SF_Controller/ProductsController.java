4eff5b2834d402c9a3b726145db12ba3a0bd4ef6
Long Hoang <longhoang12212@gmail.com>
2023-04-07 09:30:24 +0700

    Long commit

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/controller/FE_SF_Controller/ProductsController.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/controller/FE_SF_Controller/ProductsController.java
@@ -5,6 +5,7 @@ import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.ProductSF;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.dto.ProductSFDto;
 import com.codegym.springboot_modul_6.service.FE_SF_Service.IProductService;
 import com.codegym.springboot_modul_6.service.thirdpartyservice.ThirdService;
+import com.codegym.springboot_modul_6.service.thirdpartyservice.UserOnlineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -19,9 +20,13 @@ public class ProductsController {
     @Autowired
     private IProductService productService;
 
+    public UserOnlineService userOnlineService = UserOnlineService.getUserOnlineService();
+
 
     @Autowired
     private ThirdService thirdService;
+
+
 
 
     @GetMapping(value = "/{category}")
