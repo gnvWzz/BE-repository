27a5e172a6ff387f9c9ba219030362435fc030eb
Long Hoang <longhoang12212@gmail.com>
2023-04-08 08:40:50 +0700

    Commit dau ngay

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/configuration/SecurityConfig.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/configuration/SecurityConfig.java
@@ -43,8 +43,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/**");
