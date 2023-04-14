e349f7292323f18fd11af8c71a75a0a0e1a79bab
nguyenhai <nguyenhai190497@gmail.com>
2023-04-14 16:32:01 +0700

    Hải: Thêm Entity Province.java

---
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/model/FE_SF_Model/dto/ProvinceDto.java
@@ -0,0 +1,20 @@
+package com.codegym.springboot_modul_6.model.FE_SF_Model.dto;
+
+public class ProvinceDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProvinceDto() {
+    }
+
+    public ProvinceDto(String name) {
+        this.name = name;
+    }
+}
