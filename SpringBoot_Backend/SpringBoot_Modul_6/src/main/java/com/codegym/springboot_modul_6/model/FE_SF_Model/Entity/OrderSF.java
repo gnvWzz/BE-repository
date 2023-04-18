b66c1255de8957f5befefe7784ed9976581ccb34
gnvWzz <quangvuongdac2111@gmail.com>
2023-04-18 10:41:40 +0700

    Quang deleted UserDetailsServiceImpl.java, unmuted some classes and push two new entity and model

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/model/FE_SF_Model/Entity/OrderSF.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/model/FE_SF_Model/Entity/OrderSF.java
@@ -1,29 +1,29 @@
-//package com.codegym.springboot_modul_6.model.FE_SF_Model.Entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "order")
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class OrderSF {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String username;
-//
-//    @OneToMany(mappedBy = "orderSF", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
-//    private List<OrderDetailSF> orderDetailSFS;
-//
-//    private Double totalPrice;
-//}
+package com.codegym.springboot_modul_6.model.FE_SF_Model.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderSF {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @OneToMany(mappedBy = "orderSF", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<OrderDetailSF> orderDetailSFS;
+
+    private Double totalPrice;
+}
