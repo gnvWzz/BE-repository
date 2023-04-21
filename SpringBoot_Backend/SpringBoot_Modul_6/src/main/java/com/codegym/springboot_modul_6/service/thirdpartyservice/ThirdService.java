42dec4472ca4e1aae3112d658853573f5a916a2f
nguyenhai <nguyenhai190497@gmail.com>
2023-04-21 22:23:33 +0700

    Chỉnh sửa login

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/thirdpartyservice/ThirdService.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/thirdpartyservice/ThirdService.java
@@ -143,8 +143,16 @@ public class ThirdService {
     public String login(AccountDto accountDto){
         boolean isLogin = accountService.checkLogin(accountDto.getUsername(), accountDto.getPassword());
         Account account = accountService.findAccountByUsername(accountDto.getUsername()).get();
-        String role = account.getRolesList().get(0).getRoles().getName();
-        if (isLogin && (role.equals("ROLE_USER"))){
+        List<AccountRoles> roles = account.getRolesList();
+        boolean isRole  = false;
+        for (AccountRoles role :
+             roles ) {
+            if (role.getRoles().getName().equals("ROLE_USER")) {
+                isRole = true;
+                break;
+            }
+        }
+        if (isLogin && isRole){
             String jwt = jwtProvider.generateTokenLogin(account);
           return jwt;
         }
@@ -152,10 +160,18 @@ public class ThirdService {
     }
 
     public String loginOwner(AccountDto accountDto){
-        boolean isLogin = accountService.checkLogin(accountDto.getUsername(), accountDto.getPassword());
+        boolean isLogin= accountService.checkLogin(accountDto.getUsername(), accountDto.getPassword());
         Account account = accountService.findAccountByUsername(accountDto.getUsername()).get();
-        String role = account.getRolesList().get(0).getRoles().getName();
-        if (isLogin && (role.equals("ROLE_OWNER"))){
+        List<AccountRoles> roles = account.getRolesList();
+        boolean isRole  = false;
+        for (AccountRoles role :
+                roles ) {
+            if (role.getRoles().getName().equals("ROLE_OWNER")) {
+                isRole = true;
+                break;
+            }
+        }
+        if (isLogin && isRole){
 
             String jwt = jwtProvider.generateTokenLogin(account);
             return jwt;
