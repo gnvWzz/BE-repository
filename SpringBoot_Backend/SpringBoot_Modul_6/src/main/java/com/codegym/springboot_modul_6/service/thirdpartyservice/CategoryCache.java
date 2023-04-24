b95f0dc790b71a698cdc7b8a92218167e8432b88
Long Hoang <longhoang12212@gmail.com>
2023-04-24 07:49:48 +0700

    Long them chuc nang promo vao file OrderController.java

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/thirdpartyservice/CategoryCache.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/service/thirdpartyservice/CategoryCache.java
@@ -1,6 +1,8 @@
 package com.codegym.springboot_modul_6.service.thirdpartyservice;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.Categories;
+import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.Promos;
 import com.codegym.springboot_modul_6.model.FE_SF_Model.Entity.Province;
+import com.codegym.springboot_modul_6.repository.FE_SF_Repository.IPromoRepository;
 import com.codegym.springboot_modul_6.service.FE_SF_Service.ICategoryService;
 import com.codegym.springboot_modul_6.service.FE_SF_Service.ProvinceServiceIplm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ public class CategoryCache {
 
     public static final String PROVINCE = "PROVINCE";
 
+    public static final String PROMOS = "PROMOS";
+
     @Autowired
     private static final Map<String, List<?>> cacheCategories = new HashMap<>();
 
@@ -25,6 +29,9 @@ public class CategoryCache {
     private ICategoryService iCategoryService;
     @Autowired
     private ProvinceServiceIplm provinceServiceIplm;
+
+    @Autowired
+    private IPromoRepository iPromoRepository;
 
 
     private static final CategoryCache CATEGORY_CACHE = new CategoryCache();
@@ -35,10 +42,12 @@ public class CategoryCache {
 
     @PostConstruct
     public void init() {
+        List<Promos> promos = iPromoRepository.findAll();
         List<Categories> categories = (List<Categories>) iCategoryService.findAll();
         List<Province> provinces = (List<Province>) provinceServiceIplm.findAll();
         cacheCategories.put(CATEGORY, categories);
         cacheCategories.put(PROVINCE,provinces);
+        cacheCategories.put(PROMOS, promos);
     }
 
 
