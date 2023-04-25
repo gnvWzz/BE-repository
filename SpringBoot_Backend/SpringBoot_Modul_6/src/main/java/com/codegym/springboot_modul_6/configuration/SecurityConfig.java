512c30ac7fdf2bcf73aadd394889005f90df52be
TUNGHUYNH <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2023-04-25 10:37:17 +0700

    test login

--- SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/configuration/SecurityConfig.java
+++ SpringBoot_Backend/SpringBoot_Modul_6/src/main/java/com/codegym/springboot_modul_6/configuration/SecurityConfig.java
@@ -70,11 +70,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
 
-
-                .antMatchers("/api/categories/**", "/api/account/**", "/api/product/get_home").permitAll()
-                .antMatchers("/api/product/**" ).hasAnyRole("USER","OWNER")
-                .antMatchers( "/api/store/**","/api/productdetail/**","/api/price/**").hasAnyRole("OWNER")
-                .antMatchers(  "/api/cart/**", "/api/order/**").hasAnyRole("USER")
+                .antMatchers("/api/categories/**", "/api/account/**", "/api/product/get_home","/api/product/**","/api/store/**","/api/productdetail/**","/api/price/**","/api/cart/**", "/api/order/**").permitAll()
+//                .antMatchers("/api/product/**" ).hasAnyRole("USER","OWNER")
+//                .antMatchers( "/api/store/**","/api/productdetail/**","/api/price/**").hasAnyRole("OWNER")
+//                .antMatchers(  "/api/cart/**", "/api/order/**").hasAnyRole("USER")
 
                 .anyRequest().authenticated()
                 .and().csrf().disable();
